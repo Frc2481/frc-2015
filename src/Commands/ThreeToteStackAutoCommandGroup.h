e34021e527f5e0dc0366cf15a0639c16f436bb15
FRC Team 2481 <kyle@team2481.com>
2015-02-17 16:49:43 -0600

    Semi working 3 tote stack 24 seconds.

--- src/Commands/ThreeToteStackAutoCommandGroup.h
+++ src/Commands/ThreeToteStackAutoCommandGroup.h
@@ -11,6 +11,7 @@
 #include "WaitForToteCommand.h"
 #include "ArmGoToPositionCommand.h"
 #include "ArmGripperCloseCommand.h"
+#include "UnloadToteStackManualCommandGroup.h"
 
 
 
@@ -18,19 +19,62 @@ class ThreeToteStackAutoCommandGroup: public CommandGroup
 {
 public:
 	ThreeToteStackAutoCommandGroup(){
-		AddParallel(new ArmGripperCloseCommand());
-		AddParallel(new ArmGoToPositionCommand(ARM_PIVOT_POSITION_TOP));
-		AddParallel(new AcquireToteCommand());
-		AddSequential(new WaitForToteCommand());
-		AddParallel(new AutoDriveCommand(0,1,0,1.5));
-		AddParallel(new AcquireToteCommand());
-		AddSequential(new WaitForToteCommand());
-		AddSequential(new AutoMoveRCOutOfWayCommand());
-		AddParallel(new AutoDriveCommand(0,1,0,.5));
-		AddParallel(new AcquireToteCommand());
-		AddSequential(new WaitForToteCommand());
-		AddSequential(new AutoDriveCommand(1,0,0,1.5));
-		AddSequential(new UnloadToteStackCommand());
+//		AddParallel(new ArmGripperCloseCommand());
+		AddParallel(new ArmShoulderToSetPoint(2));
+//
+		AddParallel(new AutoDriveCommand(0,-.3,0,1.5));
+		AddSequential(new AcquireToteCommand());
+		AddParallel(new AutoDriveCommand(0,-.5,0));
+		AddSequential(new AcquireToteCommand());
+
+		//AddSequential(new AutoDriveCommand(0,.5,0,.25,.1));
+
+		//Reverse
+		AddSequential(new AutoDriveCommand(0,.1,0,.15));
+		AddSequential(new WaitCommand(.1));
+		AddSequential(new AutoDriveCommand(0,.5,0,.25));
+		AddSequential(new WaitCommand(.25));
+
+		//Strafe Right
+		AddSequential(new AutoDriveCommand(.1,0,0,.15));
+		AddSequential(new WaitCommand(.1));
+		AddSequential(new AutoDriveCommand(.5,0,0,.75));
+		AddSequential(new WaitCommand(.25));
+
+		//Drive Forward
+		AddSequential(new AutoDriveCommand(0,-.1,0,.15));
+		AddSequential(new WaitCommand(.1));
+		AddSequential(new AutoDriveCommand(0,-.5,0,1));
+		AddSequential(new WaitCommand(.25));
+
+		//Strafe Left
+		AddSequential(new AutoDriveCommand(-.1,0,0,.15));
+		AddSequential(new WaitCommand(.1));
+		AddSequential(new AutoDriveCommand(-.5,0,0,.95));
+		AddSequential(new WaitCommand(.25));
+
+		//Drive Forward
+		AddSequential(new AutoDriveCommand(0,-.1,.15));
+		AddSequential(new WaitCommand(.1));
+		AddParallel(new AutoDriveCommand(0,-.5,0));
+		AddSequential(new WaitCommand(.25));
+
+
+		AddSequential(new AcquireToteCommand());
+		AddSequential(new AutoDriveCommand(.5,0,0,3));
+
+		AddSequential(new UnloadToteStackCommandGroup());
+
+//		AddSequential(new WaitForToteCommand());
+//		AddParallel(new AutoDriveCommand(0,-.5,0,2.5));
+		//AddSequential(new AcquireToteCommand());
+		//AddSequential(new WaitForToteCommand());
+//		AddSequential(new AutoMoveRCOutOfWayCommand());
+//		AddParallel(new AutoDriveCommand(0,1,0,.5));
+//		AddParallel(new AcquireToteCommand());
+//		AddSequential(new WaitForToteCommand());
+//		AddSequential(new AutoDriveCommand(1,0,0,1.5));
+//		AddSequential(new UnloadToteStackCommand());
 	}
 };
 
