1aed3728aed83075713d90b6aa14fc80548fff63
FRC Team 2481 <kyle@team2481.com>
2015-02-28 10:57:40 -0600

    Added connecting batch scripts

---
+++ RobotConnect.bat
@@ -0,0 +1,2 @@
+netsh wlan connect name=2481
+netsh int ip set address "Wireless Network Connection" static 10.24.81.6 255.0.0.0
---
+++ SchoolConnect.bat
@@ -0,0 +1,2 @@
+netsh wlan connect name=THS-Public
+netsh int ip set address "Wireless Network Connection" dhcp
