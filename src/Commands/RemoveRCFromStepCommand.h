1c2e4ae6fa9aec126dd1b8d6a8753db156529613
FRC Team 2481 <kyle@team2481.com>
2015-03-03 18:02:27 -0600

    Working 1 can auto from middle step.

--- src/Commands/RemoveRCFromStepCommand.h
+++ src/Commands/RemoveRCFromStepCommand.h
@@ -4,26 +4,30 @@
 #include "Commands/CommandGroup.h"
 #include "WPILib.h"
 
-#include "ArmGoToPositionCommand.h"
+#include "ArmShoulderToSetPoint.h"
 #include "ArmGripperCloseCommand.h"
 #include "ArmGripperOpenCommand.h"
 #include "ArmExtendCommand.h"
 #include "ArmRetractCommand.h"
+#include "AutoDriveCommand.h"
 
 class RemoveRCFromStepCommand: public CommandGroup
 {
 public:
 	RemoveRCFromStepCommand() {
-		//AddSequential(new ArmGoToPositionCommand(80));
+		AddParallel(new AutoDriveCommand(0,-.2, 0,1.2));
+		AddSequential(new ArmShoulderToSetPoint(82));
 		AddSequential(new ArmGripperOpenCommand());
 		AddSequential(new ArmExtendCommand());
 		AddSequential(new WaitCommand(1));
 		AddSequential(new ArmGripperCloseCommand());
 		AddSequential(new WaitCommand(.25));
-		AddSequential(new ArmGoToPositionCommand(75));
+		AddSequential(new ArmShoulderToSetPoint(65));
 		AddSequential(new ArmRetractCommand());
-		AddSequential(new WaitCommand(.7));
-		AddSequential(new ArmGripperOpenCommand());
+		AddParallel(new AutoDriveCommand(0,.3, 0,4.5));
+		AddSequential(new ArmShoulderToSetPoint(5));
+		//AddSequential(new WaitCommand(.7));
+		//AddSequential(new ArmGripperOpenCommand());
 
 
 	}
