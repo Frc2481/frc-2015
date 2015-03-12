582d9e7837381c8dbed37325169d8ea253492666
FRC Team 2481 <kyle@team2481.com>
2015-03-12 18:39:59 -0500

    Started creating a way for co-op stack to come down from the top for unloading

--- src/Commands/StackerGoToPositionCommand.h
+++ src/Commands/StackerGoToPositionCommand.h
@@ -13,7 +13,7 @@ public:
 	StackerGoToPositionCommand(float pos)
 	:mPosition(pos){
 	}
-	void Initialize(){
+	virtual void Initialize(){
 		stacker->SetPosition(mPosition);
 		if (stacker->GetPosition() > mPosition * 578) {
 			mDown = true;
@@ -23,18 +23,56 @@ public:
 		stacker->Enable();
 	}
 	void Execute(){}
-	bool IsFinished(){
+	virtual bool IsFinished(){
 		if (mDown && stacker->GetPosition() < (mPosition * 578))
 			return true;
 		if (!mDown && stacker->GetPosition() > (mPosition * 578))
 			return true;
 		return false;
 	}
-	void End(){
+	virtual void End(){
 		stacker->Disable();
 	}
 	void Interrupted(){
 		End();
+	}
+};
+
+class StackerGoToPositionAboveStepHeightCommand : public StackerGoToPositionCommand {
+private:
+	bool mFromTop;
+public:
+	StackerGoToPositionAboveStepHeightCommand(float pos) : StackerGoToPositionCommand(pos){
+
+	}
+
+	void Initialize() {
+		if (stacker->GetPosition() > (ABOVE_STEP_HEIGHT * 578)) {
+			stacker->Enable(false);
+			mFromTop = true;
+		} else {
+			StackerGoToPositionCommand::Initialize();
+			mFromTop = false;
+		}
+	}
+
+	bool IsFinished() {
+		if (mFromTop) {
+			return stacker->GetPosition() < ((ABOVE_STEP_HEIGHT * 578) + 200);
+		} else {
+			return StackerGoToPositionCommand::IsFinished();
+		}
+	}
+
+	void End() {
+		StackerGoToPositionCommand::End();
+
+		if (mFromTop) {
+			CommandGroup* g = GetGroup();
+			if (g != NULL){
+				g->Cancel();
+			}
+		}
 	}
 };
 
